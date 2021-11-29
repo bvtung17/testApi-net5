0a5a2c0559d8847660c7cae65ac9e34bcebd0e08
Tùng Bùi <no.problem.161120@gmail.com>
2021-11-29 15:30:11 +0700

    day 1 first api

---
+++ MyWebApi/MyWebApi/Data/ChiTietDonHang.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApi.Data
+{
+    public class ChiTietDonHang
+    {
+        public Guid MaDh { get; set; }
+        public Guid MaHh { get; set; }
+        public int SoLuong { get; set; }
+        public double DonGia { get; set; }
+        public byte GiamGia { get; set; }
+        //Relationship
+        public DonHang DonHang { get; set; }
+        public HangHoa HangHoa { get; set; }
+    }
+}
---
+++ MyWebApi/MyWebApi/Migrations/20211129074846_test3.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyWebApi.Migrations
+{
+    public partial class test3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DonHang",
+                columns: table => new
+                {
+                    MaDh = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NgayDat = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2021, 11, 29, 14, 48, 45, 871, DateTimeKind.Local).AddTicks(5589)),
+                    NgayGiao = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TrangThai = table.Column<int>(type: "int", nullable: false),
+                    NguoiNhan = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DiaChi = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SoDienThoai = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DonHang", x => x.MaDh);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChiTietDonHangs",
+                columns: table => new
+                {
+                    MaDh = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MaHh = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SoLuong = table.Column<int>(type: "int", nullable: false),
+                    DonGia = table.Column<double>(type: "float", nullable: false),
+                    GiamGia = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChiTietDonHangs", x => new { x.MaDh, x.MaHh });
+                    table.ForeignKey(
+                        name: "FK_ChiTietDonHangs_DonHang_MaDh",
+                        column: x => x.MaDh,
+                        principalTable: "DonHang",
+                        principalColumn: "MaDh",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ChiTietDonHangs_HangHoa_MaHh",
+                        column: x => x.MaHh,
+                        principalTable: "HangHoa",
+                        principalColumn: "MaHh",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChiTietDonHangs_MaHh",
+                table: "ChiTietDonHangs",
+                column: "MaHh");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChiTietDonHangs");
+
+            migrationBuilder.DropTable(
+                name: "DonHang");
+        }
+    }
+}
